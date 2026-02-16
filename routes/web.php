333ff332ab0a19c86703e05f9166f63afb91585a
FFP00 <FFP00@iesemilidarder.com>
2026-02-16 01:37:10 +0100

    clase

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\{
+    Comment,
+    Image,
+    InterestingPlace,
+    Island,
+    Meeting,
+    Municipality,
+    PlaceType,
+    Role,
+    Trek,
+    User,
+    Zone
+};
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +28,39 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// Esta ruta tiene parametros obligatorios y opcionales.
+// Ten en cuenta que si es opcional pero lo devuelves tienes que asignarle tu un valor.
+// Tambien comprobamos que el obligatorio este formado por letras y no numeros.
+// Si quieres utilizarlo con route(), necesitamos name().
+Route::get('/test/{obligatorio}/{opcional?}', function($obligatorio,$opcional = "") {
+    return '<h1 style="color:red">'. $obligatorio .' Comenardo '. $opcional .'</h1>'; 
+})
+    ->where('obligatorio','^[\p{L}]+$')
+    ->name("test");
+
+// Se puede redirigir una ruta a otra ruta diferente.
+// Pero si tiene parametros tendremos que pasarlo nosotros obligatoriamente
+Route::get('/comenardo', function() {
+    return redirect()->route("test",['obligatorio' => 'Eduardo']);  
+});
+
+// Si no quieres poner el mismo prefijo 500 ves y ordenar por prefijo el codigo.
+Route::prefix('admin')->group(function () {
+
+    Route::get('/{modelo}/{id?}', function ($modelo,$id = null) {
+        $modeloMayus = ucfirst($modelo);
+        $modeloCargado = "App\\Models\\$modeloMayus";
+        if($id){
+            return $modeloCargado::findOrFail($id);  
+        }else{
+            return $modeloCargado::all();    
+        }
+        
+    });
+
 });
 
 require __DIR__.'/auth.php';
